343ca57dda130fa76b0db2ea44e27c8cf7e46526
Paul Jickling <pjickling@gmail.com>
2019-03-20 09:57:38 -0700

    Removed default css and assets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,8 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-          </p>
+          <h1>Project Match</h1>
         </header>
       </div>
     );
