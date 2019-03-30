f6800ecca4ac004a027712b0fb9c62c9ddad196b
Paul Jickling <pjickling@gmail.com>
2019-03-30 11:22:11 -0700

    ProjectList now fetches API endpoints

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -2,26 +2,50 @@ import React from 'react';
 import Project from './Project';
 
 class ProjectList extends React.Component {
-  projects() {
-    return [
-      { title: 'UXR Matching App - Digital Prototype' },
-      { title: 'ShelterTech' },
-      { title: 'Roadmap to HTML' },
-      { title: 'California OpenJustice' },
-      { title: 'The Data Science Working Group' },
-      { title: 'Brigadehub' },
-      { title: 'Adopt-a-Drain' },
-    ];
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
   }
+  componentDidMount() {
+    fetch('http://localhost:5455/api/projects')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result.data);
+          this.setState({
+            isLoaded: true,
+            projects: result.data
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }
+
   render() {
-    return (
-      <div class="container well">
-        <h1>Project List</h1>
-        {this.projects().map((project, key) =>
-          <Project key={key} {...project} />
-        )}
-      </div>
-    );
+    const { error, isLoaded, projects } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="container well">
+          <h1>Project List</h1>
+          {projects.map((project, key) =>
+            <Project key={key} {...project} />
+          )}
+        </div>
+      );
+    }
   }
 }
 
