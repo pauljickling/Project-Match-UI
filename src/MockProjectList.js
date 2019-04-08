dc98279940d8ddc015bc7ffd39479b59dd35811b
Paul Jickling <pjickling@gmail.com>
2019-04-08 10:19:31 -0700

    Fixed missing props

--- src/MockProjectList.js
+++ src/MockProjectList.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Project from './Project';
 
-class MockProjectList extends React.Component {
+class ProjectList extends React.Component {
   projects() {
     return [
-      { attributes: { name: 'Project Title', description: 'a project summary', todoItems: 'API', progressItems: 'some',
-        matchingConfig: { interests: 'civic engagement', skillsNeeded: 'React, Python', skillsOffered: 'MongoDB' }
-      },
-        attributes: { name: 'Project Title 2', description: 'a different project summary', todoItems: 'mobile app', progressItems: 'none',
-        matchingConfig: { interests: 'public transit', skillsNeeded: 'Java, Swift', skillsOffered: 'SQL, Machine Learning'}
-      },}
+      { title: 'UXR Matching App - Digital Prototype', summary: 'a summary' },
+      { title: 'ShelterTech', summary: 'summary 2' },
+      { title: 'Roadmap to HTML', summary: 'summary 3' },
+      { title: 'California OpenJustice', summary: 'summary 4' },
+      { title: 'The Data Science Working Group', summary: 'summary 5' },
+      { title: 'Brigadehub', summary: 'summary 6' },
+      { title: 'Adopt-a-Drain', summary: 'summary 7' },
     ];
   }
   render() {
     return (
-      <div className="ProjectList">
+      <div class="container well">
         <h1>Project List</h1>
         {this.projects().map((project, key) =>
           <Project key={key} {...project} />
@@ -24,4 +25,4 @@ class MockProjectList extends React.Component {
   }
 }
 
-export default MockProjectList;
+export default ProjectList;
--- src/Project.js
+++ src/Project.js
@@ -2,51 +2,24 @@ import React from 'react';
 
 class Project extends React.Component {
   render() {
-    let {
-      attributes: { name, description, todoItems, progressItems,
-        matchingConfig: { interests, skillsNeeded, skillsOffered }
-      }
-    } = this.props;
+    let { title, summary } = this.props;
     return (
-      <div className="project" style={{border: "1px solid gray", padding: 16, margin: 16}}>
-      <div className="projectCols" style={{border: "1px solid gray", display: "grid", gridGap: 10, gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))", padding: 16}}>
+      <div className="project" style={{border: "1px solid gray", padding: 8, margin: 8}}>
+      <div className="projectCols" style={{display: "grid", gridGap: 10, gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))"}}>
         <div className="projectCol1" style={{borderRight: "1px solid gray"}}>
-          <h5>{name}</h5>
-          <p>{description}</p>
+          <h5>{title}</h5>
+          <p>{summary}</p>
           <button>Contact Project Lead</button>
         </div>
         <div className="projectCol2">
           <h5>Project Needs & Interests</h5>
-          <p><strong>Skills Needed:</strong>
-          {this.attrbiutes.skillsNeeded.map(skill =>
-            <li>{skill}</li>
-            )}
-          </p>
-          <p><strong>Learning Opportunities:</strong>
-            {this.skillsOffered.map(skill =>
-              <li>{skill}</li>
-            )}
-          </p>
-          <p><strong>Civic Interests:</strong>
-            {this.interests.map(interest =>
-              <li>{interest}</li>
-            )}
-          </p>
+          <p><strong>Skills Needed:</strong></p>
+          <p><strong>Learning Opportunities:</strong></p>
+          <p><strong>Civic Interests:</strong></p>
         </div>
       </div>
         <details>
           <summary>Show project details</summary>
-            <p>Pending Tasks
-              {this.todoItems.map(item =>
-                <li>{item}</li>
-              )}
-            </p>
-            <p>Progress Made
-              {this.progressItems.map(item =>
-                <li>{item}</li>
-              )}
-            </p>
-            <p>Additional Info</p>
         </details>
       </div>
     );
